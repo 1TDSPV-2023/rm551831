bcce56404d1e0882c836fef1c645e963b1e28d96
Vitordsperera <rm551831@fiap.com.br>
2023-05-02 22:08:13 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -26,8 +26,10 @@ const aElements = document.getElementsByTagName("a");
 //Declarando um array em JavaScript
 let nr1 = [1,2,3,4,5];
 let nr2 = [6,7,8,9,10];
-
+let nr3 = [nr1, nr2];
 console.log(nr1);
 console.log(nr2);
-nr2.push(11);
-console.log(nr2);
+// console.log("NOVO ARRAY" + nr3);
+
+nr3.forEach();
+
